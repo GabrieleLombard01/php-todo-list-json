30c6ed6c4f07eb6eb72e12662b1f8d5a43b7e44b
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-20 17:37:22 +0200

    problema con API

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -1,11 +1,18 @@
 <?php
-// Initial DATA:
-
+// Recupero l'indirizzo del file che simula il databse
 $database_path = __DIR__ . '/../../database/tasks.json';
 
+// Leggo contenuto JSON
 $json_data = file_get_contents($database_path);
 
+// Converto in un'array PHP
 $tasks = json_decode($json_data, true);
+
+// SE abbiamo qualcosa in post
+$new_task = $_POST['task'] ?? null;
+if ($new_task) {
+    $tasks[] = $new_task;
+}
 
 // Avviso chi mi riceve che la risposta è in json
 header('Content-Type: application/json');
