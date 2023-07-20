fabab268b3fc6e640f2fac847bec3a87b62adfc8
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-20 16:29:36 +0200

    task in page

--- my_app/js/main.js
+++ my_app/js/main.js
@@ -1,7 +1,11 @@
 const {createApp} = Vue;
 
 const app = createApp({
-
+    data(){
+        return {
+            tasks: ["HTML", "CSS", "Responsive design", "Javascript", "PHP"]
+        }
+    }
 })
 
 app.mount('#app')
